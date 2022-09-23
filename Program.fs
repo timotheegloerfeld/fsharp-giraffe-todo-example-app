82ef99123b9d45dccd5de4a45499c7df1fda483e
Timothee Glörfeld <timothee.gloerfeld@exxeta.com>
2022-09-23 17:34:13 +0200

    implemented api

--- Program.fs
+++ Program.fs
@@ -1,176 +1,254 @@
+module TodoExample
+
 open System
-open Microsoft.AspNetCore.Builder
-open Microsoft.Extensions.Hosting
-open Giraffe
-open Npgsql.FSharp
-open Microsoft.AspNetCore.Http
+open System.Data
+open Falco
+open Falco.HostBuilder
+open Falco.Routing
+open Npgsql
+open Dapper
+open Microsoft.Extensions.Configuration
+open Microsoft.Extensions.Logging
 
-[<CLIMutable>]
-type Todo =
-    { Id: Guid
-      Text: string
-      IsChecked: bool
-      CreatedAt: DateTime
-      UpdatedAt: DateTime }
+module Service =
+    let connectionString =     
+                ConfigurationBuilder()
+                    .AddJsonFile("appsettings.json")
+                    .AddJsonFile("appsettings.Development.json")
+                    .Build()
+                    .GetConnectionString("Default") 
 
-[<CLIMutable>]
-type NewTodo = { Text: string }
+    let connectionFactory () = new NpgsqlConnection(connectionString) :> IDbConnection
 
-let TodoNotFound = $"Todo with id {id} could not be found"
+    let run 
+        (validate: 'input -> Result<'b, 'error>)
+        (provider: IDbConnection -> 'b -> Result<'c, 'error>)
+        (handleOk: ILogger -> 'c -> HttpHandler)
+        (handleError: ILogger -> 'input -> 'error -> HttpHandler)
+        (input: 'input): HttpHandler = 
+        fun ctx ->
+            use conn = connectionFactory ()
+            let log = ctx.GetLogger "Todos"
 
-let connectionString =
-    Sql.host "localhost"
-    |> Sql.database "postgres"
-    |> Sql.username "postgres"
-    |> Sql.password "mysecretpassword"
-    |> Sql.port 5432
-    |> Sql.formatConnectionString
+            let respondWith = 
+                match (validate >> Result.bind (provider conn)) input with
+                | Ok output -> handleOk log output
+                | Error error -> handleError log input error 
 
-let mapTodo (read: RowReader) : Todo =
-    { Id = read.uuid "id"
-      Text = read.string "text"
-      IsChecked = read.bool "isChecked"
-      CreatedAt = read.dateTime "createdAt"
-      UpdatedAt = read.dateTime "updatedAt" }
+            respondWith ctx
 
-let getTodoList () : Todo list =
-    connectionString
-    |> Sql.connect
-    |> Sql.query "SELECT * FROM todos;"
-    |> Sql.execute (mapTodo)
+module Result =
+    let created (obj: 'a): HttpHandler =
+        Response.withStatusCode 201 >>
+        Response.ofJson obj
 
-let insertTodo newTodo : Todo =
-    connectionString
-    |> Sql.connect
-    |> Sql.query "INSERT INTO todos (text) VALUES (@text) RETURNING *;"
-    |> Sql.parameters [ ("text", Sql.string newTodo.Text) ]
-    |> Sql.executeRow (mapTodo)
+    let badRequest: HttpHandler =
+        Response.withStatusCode 400 >>
+        Response.ofPlainText "Bad request"
 
-let getTodoById id : Result<Todo, string> =
-    let todos =
-        connectionString
-        |> Sql.connect
-        |> Sql.query "SELECT * FROM todos where id = @id"
-        |> Sql.parameters [ ("id", Sql.uuid id) ]
-        |> Sql.execute (mapTodo)
+    let notFound: HttpHandler =
+        Response.withStatusCode 404 >>
+        Response.ofPlainText "Not found"
 
-    match todos with
-    | [] -> Error TodoNotFound
-    | _ -> Ok todos[0]
+    let noContent: HttpHandler =
+        Response.withStatusCode 204 >>
+        Response.ofEmpty
 
-let deleteTodo id : Result<unit, string> =
-    let rows =
-        connectionString
-        |> Sql.connect
-        |> Sql.query "DELETE FROM todos WHERE id = @id"
-        |> Sql.parameters [ ("id", Sql.uuid id) ]
-        |> Sql.executeNonQuery
+module Todos = 
+    type Todo = 
+        { Id: Guid
+        ; Text: string
+        ; IsChecked: bool
+        ; CreatedAt: DateTime
+        ; UpdatedAt: DateTime }
 
-    match rows with
-    | 0 -> Error TodoNotFound
-    | _ -> Ok()
+    type Error =
+        | InvalidText 
+        | InvalidId
+        | TodoNotFound
 
-let checkTodo id : Result<unit, string> =
-    let rows =
-        connectionString
-        |> Sql.connect
-        |> Sql.query "UPDATE todos SET \"isChecked\" = true WHERE id = @id"
-        |> Sql.parameters [ ("id", Sql.uuid id) ]
-        |> Sql.executeNonQuery
+    let handleError (log: ILogger) input error = 
+                match error with
+                | InvalidText | InvalidId ->
+                    log.LogError $"Invalid input {input} has been submitted"
+                    Result.badRequest
+                | TodoNotFound -> 
+                    log.LogInformation $"Todo with id {input} could not be found"
+                    Result.notFound
 
-    match rows with
-    | 0 -> Error TodoNotFound
-    | _ -> Ok()
+    module New = 
+        type newTodo = { Text: String }
 
-let uncheckTodo id : Result<unit, string> =
-    let rows =
-        connectionString
-        |> Sql.connect
-        |> Sql.query "UPDATE todos SET \"isChecked\" = false WHERE id = @id"
-        |> Sql.parameters [ ("id", Sql.uuid id) ]
-        |> Sql.executeNonQuery
+        let mutation 
+            (conn: IDbConnection)
+            (newTodo: newTodo) = 
+            let sql = "
+                INSERT INTO todos (text) 
+                VALUES (@Text) 
+                RETURNING *;"
 
-    match rows with
-    | 0 -> Error TodoNotFound
-    | _ -> Ok()
+            let data = {| text = newTodo.Text |}
+            Ok (conn.QueryFirst<Todo>(sql, data))
 
-let handleGetTodos: HttpHandler =
-    fun (next: HttpFunc) (ctx: HttpContext) ->
-        let todoList = getTodoList ()
-        json todoList next ctx
+        let validate (newTodo: newTodo) =
+            match newTodo.Text with
+            | "" -> Error InvalidText
+            | null -> Error InvalidText
+            | _ -> Ok newTodo
 
-let handleGetTodoById id : HttpHandler =
-    fun (next: HttpFunc) (ctx: HttpContext) ->
-        let dbResult = getTodoById id
+        let handle =   
+            let handleOk _ (todo: Todo): HttpHandler = 
+                Result.created todo
 
-        match dbResult with
-        | Error msg ->
-            ctx.SetStatusCode 404
-            text msg next ctx
-        | Ok todo -> json todo next ctx
+            Request.mapJson (Service.run validate mutation handleOk handleError)
 
-// TODO add validation, empty request bodies lead to todos created without
-// name currently
-let handlePostTodo: HttpHandler =
-    fun (next: HttpFunc) (ctx: HttpContext) ->
-        task {
-            let! newTodo = ctx.BindJsonAsync<NewTodo>()
-            let todo = insertTodo newTodo
-            ctx.SetStatusCode 201
-            ctx.SetHttpHeader("Location", $"{ctx.GetRequestUrl()}/{todo.Id}")
-            return! json todo next ctx
+    module GetAll =               
+        let query 
+            (conn: IDbConnection)
+            _ = 
+            let sql = "SELECT * FROM todos;"
+            Ok (conn.Query<Todo>(sql))
+
+        let validate _ = Ok () 
+        
+        let handle =
+            let handleOk _ todos = 
+                Response.ofJson todos
+            
+            Service.run validate query handleOk handleError ()
+
+    module Get =
+        let query 
+            (conn: IDbConnection)
+            (id: Guid) = 
+            let sql = "
+                SELECT * FROM todos
+                WHERE id = (@Id);"
+
+            let data = {| id = id |}
+            match box (conn.QueryFirstOrDefault<Todo>(sql, data)) with 
+            | null -> Error TodoNotFound
+            | todo -> Ok (unbox todo)
+
+        let validate (idOption: Guid option) =
+            match idOption with
+            | Some id -> Ok id
+            | None -> Error InvalidId
+
+        let handle: HttpHandler =
+            let routeMap (route : RouteCollectionReader) = 
+                route.TryGetGuid "id"
+
+            let handleOk _ (todo: Todo) =  
+                todo |>
+                Response.ofJson
+
+            Request.mapRoute routeMap (Service.run validate query handleOk handleError)
+
+    module Delete = 
+        let mutation
+            (conn: IDbConnection) 
+            (id: Guid) = 
+            let sql = "
+                DELETE FROM todos
+                WHERE id = (@Id);"
+
+            let data = {| id = id |}
+            let changed = conn.Execute(sql, data)
+
+            match changed with 
+            | 0 -> Error TodoNotFound
+            | _ -> Ok ()
+
+        let validate (idOption: Guid option) = 
+            match idOption with
+            | Some id -> Ok id
+            | None -> Error InvalidId
+
+        let handle: HttpHandler = 
+            let routeMap (route : RouteCollectionReader) = 
+                route.TryGetGuid "id"
+
+            let handleOk _ () =
+                Result.noContent
+                
+            Request.mapRoute routeMap (Service.run validate mutation handleOk handleError)
+    
+    module Check = 
+        let mutation
+            (conn: IDbConnection) 
+            (id: Guid) = 
+            let sql = "
+                UPDATE todos
+                SET \"isChecked\" = true
+                WHERE id = (@Id);"
+
+            let data = {| id = id |}
+            let changed = conn.Execute(sql, data)
+
+            match changed with 
+            | 0 -> Error TodoNotFound
+            | _ -> Ok ()
+
+        let validate (idOption: Guid option) = 
+            match idOption with
+            | Some id -> Ok id
+            | None -> Error InvalidId
+
+        let handle: HttpHandler = 
+            let routeMap (route : RouteCollectionReader) = 
+                route.TryGetGuid "id"
+
+            let handleOk _ () =
+                Result.noContent
+                
+            Request.mapRoute routeMap (Service.run validate mutation handleOk handleError)
+
+    module Uncheck = 
+        let mutation
+            (conn: IDbConnection) 
+            (id: Guid) = 
+            let sql = "
+                UPDATE todos
+                SET \"isChecked\" = false
+                WHERE id = (@Id);"
+
+            let data = {| id = id |}
+            let changed = conn.Execute(sql, data)
+            match changed with 
+            | 0 -> Error TodoNotFound
+            | _ -> Ok ()
+
+        let validate (idOption: Guid option) = 
+            match idOption with
+            | Some id -> Ok id
+            | None -> Error InvalidId
+
+        let handle: HttpHandler = 
+            let routeMap (route : RouteCollectionReader) = 
+                route.TryGetGuid "id"
+
+            let handleOk _ () =
+                Result.noContent
+                
+            Request.mapRoute routeMap (Service.run validate mutation handleOk handleError)
+
+module Program = 
+    [<EntryPoint>]
+    let main args =
+        webHost args {
+            endpoints [ 
+                all "/todos" 
+                    [ POST, Todos.New.handle
+                    ; GET, Todos.GetAll.handle ]
+
+                all "/todos/{id:guid}" 
+                    [ GET, Todos.Get.handle
+                    ; DELETE, Todos.Delete.handle ]
+
+                all "/todos/{id:guid}/check" 
+                    [ POST, Todos.Check.handle
+                    ; DELETE, Todos.Uncheck.handle ] 
+            ]
         }
-
-let handleDeleteTodo id : HttpHandler =
-    fun (next: HttpFunc) (ctx: HttpContext) ->
-        let dbResult = deleteTodo id
-
-        match dbResult with
-        | Error msg ->
-            ctx.SetStatusCode 404
-            text msg next ctx
-        | Ok _ ->
-            ctx.SetStatusCode 204
-            next ctx
-
-let handlePostTodoCheck id : HttpHandler =
-    fun (next: HttpFunc) (ctx: HttpContext) ->
-        let dbResult = checkTodo id
-
-        match dbResult with
-        | Error msg ->
-            ctx.SetStatusCode 404
-            text msg next ctx
-        | Ok _ ->
-            ctx.SetStatusCode 204
-            next ctx
-
-let handleDeleteTodoCheck id : HttpHandler =
-    fun (next: HttpFunc) (ctx: HttpContext) ->
-        let dbResult = uncheckTodo id
-
-        match dbResult with
-        | Error msg ->
-            ctx.SetStatusCode 404
-            text msg next ctx
-        | Ok _ ->
-            ctx.SetStatusCode 204
-            next ctx
-
-
-let webApp =
-    choose [ route "/todos"
-             >=> choose [ GET >=> handleGetTodos
-                          POST >=> handlePostTodo ]
-             routef "/todos/%O" (fun id ->
-                 (choose [ GET >=> handleGetTodoById id
-                           DELETE >=> handleDeleteTodo id ]))
-             routef "/todos/%O/check" (fun id ->
-                 (choose [ POST >=> handlePostTodoCheck id
-                           DELETE >=> handleDeleteTodoCheck id ])) ]
-
-let builder = WebApplication.CreateBuilder()
-builder.Services.AddGiraffe() |> ignore
-let app = builder.Build()
-app.UseGiraffe webApp
-app.Run()
+        0
